7737105ea77c601c0b7957a5a574bba7dada5b20
imane <bellaghma.imane@gmail.com>
2024-02-15 22:54:30 +0100

    added

---
+++ main.js
@@ -0,0 +1,13 @@
+let day=document.querySelector(".inDay")
+let  month =document.querySelector(".inMonth")
+let year=document.querySelector(".inYear")
+
+let clickBtn=document.querySelector(".btn")
+
+
+function oni(){
+let  day=document.querySelector(".inDay").value;
+    document.getElementById("hidden").innerHTML = "u wrote" +day;
+}
+
+
