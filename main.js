08773209437990719f76b421b31b922d3a257209
imane <bellaghma.imane@gmail.com>
2024-02-17 15:30:08 +0100

    modify

--- main.js
+++ main.js
@@ -2,12 +2,38 @@ let day=document.querySelector(".inDay")
 let  month =document.querySelector(".inMonth")
 let year=document.querySelector(".inYear")
 
+//let span=document.querySelector(".hidden")
+
+
+let yearsBefore = document.querySelectorAll(".years");
+let monthsBefore = document.querySelectorAll(".months");
+let daysBefore = document.querySelectorAll(".days");
 let clickBtn=document.querySelector(".btn")
 
+let ey = document.querySelectorAll(".erreurYear");
+let em= document.querySelectorAll(".erreurMonth");
+let ed = document.querySelectorAll(".erreurDay");
 
+
+clickBtn.addEventListener("click", () => {
+    span.classList.add("hidden")
+
+   
+})
+clickBtn.addEventListener("click", () => {
+    span.classList.remove("hidden")
+  
+})
+
+
+
+/*
 function oni(){
-let  day=document.querySelector(".inDay").value;
-    document.getElementById("hidden").innerHTML = "u wrote" +day;
-}
+let textday= day.value;
+
+let  spanRes =document.getElementById("hidden")
+
+spanRes.innerHTML = textday;
+}*/
 
 
